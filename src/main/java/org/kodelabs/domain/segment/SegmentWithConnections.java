6b05f561b6599dab606f8cbcd143ebe608efbd85
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-03 16:27:20 +0200

    delete segment package

--- src/main/java/org/kodelabs/domain/segment/SegmentWithConnections.java
+++
@@ -1,9 +0,0 @@
-package org.kodelabs.domain.segment;
-
-import org.kodelabs.domain.segment.entity.SegmentEntity;
-
-import java.util.List;
-
-public class SegmentWithConnections extends SegmentEntity {
-    public List<SegmentEntity> connections;
-}
