0c0d2e959bdbba991c8ddd727ec97714f6ff30cf
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-04 11:18:47 +0200

    - add support for loading airports fixture - rename FlightGeneratorService.java to FixtureLoaderService - refactor MongoConfig, extract common code in a new method

--- src/main/java/org/kodelabs/domain/common/MongoConfig.java
+++ src/main/java/org/kodelabs/domain/common/MongoConfig.java
@@ -7,6 +7,7 @@ import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kodelabs.domain.airport.entity.AirportEntity;
 import org.kodelabs.domain.flight.entity.FlightEntity;
 
 @ApplicationScoped
@@ -24,15 +25,34 @@ public class MongoConfig {
     @ConfigProperty(name = "app.mongodb.flight-collection")
     String flightCollection;
 
+    @ConfigProperty(name = "app.mongodb.airport-collection")
+    String airportCollection;
+
+    public ReactiveMongoCollection<FlightEntity> getFlightCollection() {
+        return client.getDatabase(database).getCollection(flightCollection, FlightEntity.class);
+    }
+
+    public <T> ReactiveMongoCollection<T> getFlightCollection(Class<T> entityClass) {
+        return client.getDatabase(database).getCollection(flightCollection, entityClass);
+    }
+
     public MongoCollection<FlightEntity> getNonReactiveFlightCollection() {
-        return nonReactiveClient
-                .getDatabase(database)
-                .getCollection(flightCollection, FlightEntity.class);
+        return getNonReactiveCollection(flightCollection, FlightEntity.class);
+    }
+
+    public MongoCollection<AirportEntity> getNonReactiveAirportCollection() {
+        return getNonReactiveCollection(airportCollection, AirportEntity.class);
     }
 
     public <T> ReactiveMongoCollection<T> getCollection(String collectionName,
                                                         Class<T> entityClass) {
         return client.getDatabase(database)
+                .getCollection(collectionName, entityClass);
+    }
+
+    public <T> MongoCollection<T> getNonReactiveCollection(String collectionName,
+                                                           Class<T> entityClass) {
+        return nonReactiveClient.getDatabase(database)
                 .getCollection(collectionName, entityClass);
     }
 }
---
+++ src/main/java/org/kodelabs/domain/startup/FixtureLoaderService.java
@@ -0,0 +1,98 @@
+package org.kodelabs.domain.startup;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoCollection;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import org.kodelabs.domain.airport.entity.AirportEntity;
+import org.kodelabs.domain.common.MongoConfig;
+import org.kodelabs.domain.flight.entity.FlightEntity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.*;
+import java.util.List;
+
+@ApplicationScoped
+public class FixtureLoaderService {
+
+    @Inject
+    MongoConfig mongoConfig;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    MongoCollection<FlightEntity> flightCollection;
+
+    MongoCollection<AirportEntity> airportCollection;
+
+
+    public void onStart(@Observes StartupEvent ev) {
+        flightCollection = mongoConfig.getNonReactiveFlightCollection();
+        airportCollection = mongoConfig.getNonReactiveAirportCollection();
+
+        try (InputStream flightIs = getClass().getResourceAsStream("/fixtures/flights.json");
+             InputStream airportsIs = getClass().getResourceAsStream("/fixtures/airports.json");
+        ) {
+            populateFlights(flightIs);
+            populateAirportCollection(airportsIs);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load json file", e);
+        }
+    }
+
+    private void populateFlights(InputStream flightIs) throws IOException {
+        if (flightIs == null) {
+            throw new IllegalStateException("Flights.json not found");
+        }
+
+        List<FlightEntity> flightEntities = objectMapper
+                .readerFor(new TypeReference<List<FlightEntity>>() {
+                })
+                .without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .readValue(flightIs);
+
+
+        for (FlightEntity flight : flightEntities) {
+            flight.setCreatedAt(Instant.now());
+            flight.setUpdatedAt(Instant.now());
+
+            if (flightCollection.find(new org.bson.Document("_id", flight.getId())).first() == null) {
+                flightCollection.insertOne(flight);
+                System.out.println("Inserted flight " + flight.getId());
+            } else {
+                System.out.println("Skipping duplicate flight " + flight.getId());
+            }
+        }
+    }
+
+    private void populateAirportCollection(InputStream airportsIs) throws IOException {
+        if (airportsIs == null) {
+            throw new IllegalStateException("Airport.json not found");
+        }
+
+        List<AirportEntity> airportEntities = objectMapper
+                .readerFor(new TypeReference<List<AirportEntity>>() {
+                })
+                .without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .readValue(airportsIs);
+
+
+        for (AirportEntity airport : airportEntities) {
+            airport.createdAt = Instant.now();
+            airport.updatedAt = Instant.now();
+
+            if (airportCollection.find(new org.bson.Document("_id", airport.getId())).first() == null) {
+                airportCollection.insertOne(airport);
+                System.out.println("Inserted airport " + airport.getId());
+            } else {
+                System.out.println("Skipping duplicate airport " + airport.getId());
+            }
+        }
+    }
+}
