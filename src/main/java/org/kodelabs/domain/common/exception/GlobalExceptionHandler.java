28163260cf0ef881d27fdc34a7f938beecdfe24e
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-05 09:59:59 +0200

    add a GlobalExceptionHandler

---
+++ src/main/java/org/kodelabs/domain/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package org.kodelabs.domain.common.exception;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import org.jboss.logging.Logger;
+
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
+
+    private static final Logger LOG = Logger.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        LOG.error("Exception handled globally: ", exception);
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(new ErrorResponse("Unexpected error", 500))
+                .build();
+    }
+
+    record ErrorResponse(String message, int code) {}
+}
