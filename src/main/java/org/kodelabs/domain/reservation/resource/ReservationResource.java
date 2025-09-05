8581012ce02fbf34b8cb0367d52423ff58c0eb15
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-05 15:26:35 +0200

    feat(api): add ReservationResource

---
+++ src/main/java/org/kodelabs/domain/reservation/resource/ReservationResource.java
@@ -0,0 +1,30 @@
+package org.kodelabs.domain.reservation.resource;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.kodelabs.domain.reservation.dto.CreateReservationDTO;
+import org.kodelabs.domain.reservation.entity.ReservationEntity;
+import org.kodelabs.domain.reservation.service.ReservationService;
+
+@Path("/reservations")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ReservationResource {
+
+    @Inject
+    ReservationService reservationService;
+
+    @GET
+    @Path("/{id}")
+    public Uni<ReservationEntity> findByObjectId(@PathParam("id") String id) {
+        return reservationService.findByObjectId(id);
+    }
+
+    @POST
+    public Uni<ReservationEntity> crateReservation(@Valid CreateReservationDTO reservationDTO) {
+        return reservationService.createReservationTemp(reservationDTO);
+    }
+}
