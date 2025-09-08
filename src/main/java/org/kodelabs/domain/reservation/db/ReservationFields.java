8ca906417be68d74e5724811adc403a99c4277f5
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-08 15:54:41 +0200

    feat(api) Add ReservationField class, it holds field names for reservation documents

---
+++ src/main/java/org/kodelabs/domain/reservation/db/ReservationFields.java
@@ -0,0 +1,6 @@
+package org.kodelabs.domain.reservation.db;
+
+public class ReservationFields {
+    public static final String ID = "_id";
+    public static final String USER_ID = "userId";
+}
--- src/main/java/org/kodelabs/domain/reservation/mongo/ReservationFields.java
+++
@@ -1,5 +0,0 @@
-package org.kodelabs.domain.reservation.mongo;
-
-public class ReservationFields {
-    public static final String ID = "_id";
-}
