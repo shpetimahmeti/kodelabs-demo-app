0f70c85bbebba2eded78dcf6b1cb2c50fc321581
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-08 16:44:41 +0200

    feat(api) Add ReservationDTO

---
+++ src/main/java/org/kodelabs/domain/reservation/dto/ReservationDTO.java
@@ -0,0 +1,33 @@
+package org.kodelabs.domain.reservation.dto;
+
+public class ReservationDTO {
+
+    private String id;
+    private String userId;
+    private String flightId;
+    private String seatNumber;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+}
