4dbb6520ba0d0e22dffc31ed022e71beaa7c9dcc
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-08 15:55:35 +0200

    feat(api) Add AirportPaginationFacetResult

---
+++ src/main/java/org/kodelabs/domain/airport/dto/AirportPaginationFacetResult.java
@@ -0,0 +1,7 @@
+package org.kodelabs.domain.airport.dto;
+
+import org.kodelabs.domain.airport.entity.AirportEntity;
+import org.kodelabs.domain.common.dto.PaginationFacetResult;
+
+public class AirportPaginationFacetResult extends PaginationFacetResult<AirportEntity> {
+}
