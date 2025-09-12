e7330c1fcee6bbf2fe4c21a89e9b0846fb6da878
shpetimahmeti <shpetimahmeti@kodelabs.com>
2025-09-12 10:00:27 +0200

    refactor(flight): Update FlightStatus enum, override how json resolves enum values and throw exception if value not found

--- src/main/java/org/kodelabs/domain/flight/enums/FlightStatus.java
+++ src/main/java/org/kodelabs/domain/flight/enums/FlightStatus.java
@@ -1,5 +1,6 @@
 package org.kodelabs.domain.flight.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum FlightStatus {
@@ -13,5 +14,16 @@ public enum FlightStatus {
     @JsonValue
     public String toValue() {
         return name().toLowerCase();
+    }
+
+    @JsonCreator
+    public static FlightStatus fromValue(String value) {
+        for (FlightStatus status : values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid flight status: " + value);
     }
 }
